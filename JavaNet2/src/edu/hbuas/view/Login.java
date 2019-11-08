5907dc031043c26e143caa9d86ba594c831826e9
Lenovo <Lenovo@dsj-45>
2019-11-08 11:03:48 +0800

    first commit

---
+++ JavaNet2/src/edu/hbuas/view/Login.java
@@ -0,0 +1,18 @@
+package edu.hbuas.view;
+
+import javax.swing.JFrame;
+
+public class Login extends JFrame {
+	
+	public Login() {
+		setTitle("login");
+		setLocationRelativeTo(null);
+		setSize(300, 200);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new Login();
+	}
+
+}
